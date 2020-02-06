c725a273768b9acc65ab4e81ad95f7f73c663325
afew3232 <afew3232@gmail.com>
2020-02-06 14:24:15 +0900

    KADAI_8_2.6.13:00

--- Bookers2/app/controllers/books_controller.rb
+++ Bookers2/app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
   	@books = Book.all
+    @books_error = Book.new
   	@book = Book.new
   end
 
@@ -11,21 +12,31 @@ class BooksController < ApplicationController
 
   def edit
   	@book = Book.find(params[:id])
+    @book_error = Book.find(params[:id])
   end
 
   def root
   end
 
   def create
-  	book = Book.new(book_params)
-  	book.save
+  	@books = Book.new(book_params)
+  	if @books.save
   	#puts params[:id] nilが返ってくる? URLに:idがないルートを通ると
   	#                 nilが返ってくる?
   	#rake routesだと show edit update destroyのみ
   	#puts book.id 新しく作成したbookのidが入ってる
   	#route変更でも対応できる？？
-  	flash[:notice] = "Book was successfully created"
-  	redirect_to book_path(book.id)
+  	 flash[:notice] = "Book was successfully created"
+  	 redirect_to book_path(@books.id)
+    else
+       #book.errors.full_messages.each do |ms|
+       #end
+       @books_error = @books
+       @books = Book.all
+       @book = Book.new
+      render :index
+      return
+    end
   end
 
   def destroy
@@ -37,9 +48,14 @@ class BooksController < ApplicationController
 
   def update
   	book = Book.find(params[:id])
-  	book.update(book_params)
-  	flash[:notice] = "Book was successfully updated"
-  	redirect_to book_path(params[:id])
+  	if book.update(book_params)
+  	 flash[:notice] = "Book was successfully updated"
+  	 redirect_to book_path(params[:id])
+    else
+       @book_error = book
+       @book = book
+       render :edit
+    end
   end
 
 
